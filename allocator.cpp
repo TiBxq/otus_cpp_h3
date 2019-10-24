795c1e59b8b78d3edc7d2e862c3dccde13219e27
Bogdan 'TiBx' Savenko <savenko.bogdan@gmail.com>
2019-10-25 00:12:19 +0300

    added more logging

--- allocator.cpp
+++ allocator.cpp
@@ -24,18 +24,32 @@ struct custom_allocator
 		using other = custom_allocator<U>;
 	};
 
-	custom_allocator() = default;
-	~custom_allocator() = default;
+	custom_allocator()
+	{
+#ifdef ENABLE_LOGGING
+		std::cout << "ctor " << typeid(T).name() << std::endl;
+#endif
+	}
+
+	~custom_allocator()
+	{
+#ifdef ENABLE_LOGGING
+		std::cout << "dtor " << typeid(T).name() << std::endl;
+#endif
+	}
 
 	template<typename U>
 	custom_allocator(const custom_allocator<U>&)
 	{
+#ifdef ENABLE_LOGGING
+		std::cout << "copy ctor " << typeid(T).name() << std::endl;
+#endif
 	}
 
 	T* allocate(std::size_t n)
 	{
 #ifdef ENABLE_LOGGING
-		std::cout << "allocate: [n = " << n << "]" << std::endl;
+		std::cout << "allocate: [n = " << n << "] " << typeid(T).name() << std::endl;
 #endif
 
 		auto p = std::malloc(sizeof(T) * n);
@@ -49,7 +63,7 @@ struct custom_allocator
 	void deallocate(T* p, std::size_t n)
 	{
 #ifdef ENABLE_LOGGING
-		std::cout << "deallocate: [n = " << n << "]" << std::endl;
+		std::cout << "deallocate: [n = " << n << "] " << typeid(T).name() << std::endl;
 #endif
 
 		std::free(p);
@@ -59,7 +73,7 @@ struct custom_allocator
 	void construct(U* p, Args&& ...args)
 	{
 #ifdef ENABLE_LOGGING
-		std::cout << "construct" << std::endl;
+		std::cout << "construct " << typeid(T).name() << std::endl;
 #endif
 
 		new(p) U(std::forward<Args>(args)...);
@@ -68,7 +82,7 @@ struct custom_allocator
 	void destroy(T* p)
 	{
 #ifdef ENABLE_LOGGING
-		std::cout << "destroy" << std::endl;
+		std::cout << "destroy " << typeid(T).name() << std::endl;
 #endif
 
 		p->~T();
@@ -86,12 +100,12 @@ int main(/*int argc, char const *argv[]*/)
 	//------
 	std::cout << "map: " << std::endl;
 
-	auto m = std::map<int, int, std::less<int>, custom_allocator<int>>();
+	auto m = std::map<int, int, std::less<int>, custom_allocator<std::pair<int, int>>>();
 	m[3] = 14;
 
 	std::cout << m[3] << std::endl;
 
 	//------
-	std::cout << std::endl << "end of main" << std::endl;
+	std::cout << std::endl << "*** end of main ***" << std::endl << std::endl;
     return 0;
 }
