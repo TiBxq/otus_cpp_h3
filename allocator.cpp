e3072a823c9e59ea71d589ef9f1383fb0fa40563
Bogdan 'TiBx' Savenko <savenko.bogdan@gmail.com>
2019-11-12 01:00:47 +0200

    make custom_allocator allocate memory once

--- allocator.cpp
+++ allocator.cpp
@@ -7,9 +7,9 @@
 #include <algorithm>
 #include <typeinfo>
 
-#define ENABLE_LOGGING 1
+//#define ENABLE_LOGGING 1
 
-template<typename T>
+template<typename T, std::size_t N>
 struct custom_allocator
 {
 	using value_type = T;
@@ -22,7 +22,7 @@ struct custom_allocator
 	template<typename U>
 	struct rebind
 	{
-		using other = custom_allocator<U>;
+		using other = custom_allocator<U, N>;
 	};
 
 	custom_allocator()
@@ -30,6 +30,8 @@ struct custom_allocator
 #ifdef ENABLE_LOGGING
 		std::cout << "ctor " << typeid(T).name() << std::endl;
 #endif
+		m_memory = reinterpret_cast<T*>(std::malloc(sizeof(T) * N));
+		m_index = 0;
 	}
 
 	~custom_allocator()
@@ -37,10 +39,11 @@ struct custom_allocator
 #ifdef ENABLE_LOGGING
 		std::cout << "dtor " << typeid(T).name() << std::endl;
 #endif
+		std::free(m_memory);
 	}
 
 	template<typename U>
-	custom_allocator(const custom_allocator<U>&)
+	custom_allocator(const custom_allocator<U, N>&)
 	{
 #ifdef ENABLE_LOGGING
 		std::cout << "copy ctor " << typeid(T).name() << std::endl;
@@ -52,23 +55,22 @@ struct custom_allocator
 #ifdef ENABLE_LOGGING
 		std::cout << "allocate: [n = " << n << "] " << typeid(T).name() << std::endl;
 #endif
-
-		auto p = std::malloc(sizeof(T) * n);
-		if (!p)
-		{
-			throw std::bad_alloc();
-		}
-		return reinterpret_cast<T*>(p);
+		auto p = m_memory + m_index;
+		m_index += n;
+		return p;
 	}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 
 	void deallocate(T* p, std::size_t n)
 	{
 #ifdef ENABLE_LOGGING
 		std::cout << "deallocate: [n = " << n << "] " << typeid(T).name() << std::endl;
 #endif
-
-		std::free(p);
 	}
+
+#pragma GCC diagnostic pop
 
 	template<typename U, typename ...Args>
 	void construct(U* p, Args&& ...args)
@@ -76,7 +78,6 @@ struct custom_allocator
 #ifdef ENABLE_LOGGING
 		std::cout << "construct " << typeid(T).name() << std::endl;
 #endif
-
 		new(p) U(std::forward<Args>(args)...);
 	}
 
@@ -85,28 +86,36 @@ struct custom_allocator
 #ifdef ENABLE_LOGGING
 		std::cout << "destroy " << typeid(T).name() << std::endl;
 #endif
-
 		p->~T();
 	}
+
+//private:
+	T* m_memory;
+	int m_index;
 };
 
 int main(/*int argc, char const *argv[]*/)
 {
-	std::cout << "vector: " << std::endl;
+	//std::cout << "vector: " << std::endl;
 
-	auto v = std::vector<int, custom_allocator<int>>();
+	auto v = std::vector<int, custom_allocator<int, 5>>();
+	v.push_back(5);
+	v.push_back(5);
+	v.push_back(5);
+	v.push_back(5);
 	v.push_back(5);
 
-	std::cout << v[0] << std::endl;
-	//------
-	std::cout << "map: " << std::endl;
 
-	auto m = std::map<int, int, std::less<int>, custom_allocator<std::pair<const int, int>>>();
+	//std::cout << v[8] << std::endl;
+	//------
+	/*std::cout << "map: " << std::endl;
+
+	auto m = std::map<int, int, std::less<int>, custom_allocator<std::pair<const int, int>, 5>>();
 	m[3] = 14;
 
 	std::cout << m[3] << std::endl;
 
-	//------
-	std::cout << std::endl << "*** end of main ***" << std::endl << std::endl;
+	//------*/
+	//std::cout << std::endl << "*** end of main ***" << std::endl << std::endl;
     return 0;
 }
